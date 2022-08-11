f1ca5b41566baa84019e526b36662b2ef19bb375
Ozan <ozancetin6@hotmail.com>
2022-08-11 10:52:11 +0300

    adding new urlpatterns

--- films/urls.py
+++ films/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    # /films/
     path('', views.index, name="index"), 
+    # /films/5/
+    path('<slug:film_title>/', views.detail, name='detail'),
+    # /films/5/results/
+    path('<slug:film_title>/results', views.results, name='results'), 
+    # /films/5/vote/
+    path('<slug:film_title>/vote/', views.vote, name='vote')
 ]
--- films/views.py
+++ films/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request): 
     return HttpResponse("Hello world, you're in the bibliopole")
+def detail(request, film_title): 
+    return HttpResponse("You're looking at film " + film_title)
+def results(request, film_title): 
+    response = "Your're looking at the results of question %s."
+    return HttpResponse(response + film_title)
+def vote(request, film_title): 
+    return HttpResponse("You're voting on film " + film_title)
